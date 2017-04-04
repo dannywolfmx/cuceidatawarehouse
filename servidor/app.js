57d0e4f49470396eda605657d7a0d57c111d9696
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2017-04-04 16:49:26 -0500

    Ahora muestra el array en la misma pagina

--- servidor/app.js
+++ servidor/app.js
@@ -1,40 +1,59 @@
 const express = require('express'),
     app = express(),
     file = require("fs"),
-    fileUpload = require("express-fileupload"),
-    node_xj = require("xls-to-json"),
-    PDFParser = require("pdf2json")
-    pdf2table = require("pdf2table"),
-    csvtojson = require("csvtojson"),
-    xml2js = require("xml2js"),
-    xlscToJson = require("xlsx-to-json");
+    fileUpload = require    ( "express-fileupload" ),
+    node_xj     = require   (   "xls-to-json"   ),
+    PDFParser   = require   (   "pdf2json"      )
+    pdf2table   = require   (   "pdf2table"     ),
+    csvtojson   = require   (   "csvtojson"     ),
+    xml2js      = require   (   "xml2js"        ),
+    xlscToJson  = require   (   "xlsx-to-json"  );
+
+const dataBase = []
 
 const PUERTO = 3001;
 
 app.use(fileUpload());
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + './public/bower_components'));
+
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://127.0.0.1:3001', 'http://localhost:3001', 'http://danielmenchaca:3001'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
 
 app.post('/upload',(req,res) =>{
     if(!req.files){
-        return res.status(400).send("No files were uploaded");
+        return res.status(400).send("No se encontro archivo para subir");
     }
 
-    let archivo = req.files.sampleFile;
+    console.log(req.files.file)
+
+    let archivo = req.files.file;
     let nombreArchivo = archivo.name
     let tipo = archivo.mimetype
-    console.log(tipo)
+    
+    
+    //console.log(tipo)
     archivo.mv(nombreArchivo,(err)=>{
-        if(err){
-            return res.status(500).send(err)
+        if(err){ 
+            console.log(err)
+            return res.status(500).send(err) 
         }
         
         if("application/vnd.ms-excel" == tipo){
             convertirXls(nombreArchivo,res)
         }else if("application/pdf" == tipo){
-
-            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
             convertirPdf(nombreArchivo,res)
         }else if("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" == tipo){
             convertirXLSX(nombreArchivo,res)
@@ -46,12 +65,7 @@ app.post('/upload',(req,res) =>{
         }else{
             res.send("Tipo desconocido");
         }
-
     })
-
-
-
-    
 })
 
 
@@ -61,6 +75,11 @@ app.set('port', process.env.PORT || PUERTO);
 
 app.get('/',(req,res) => {
     res.sendFile("./index.html")
+})
+
+app.get('/array',(req,res) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.send(dataBase[0])
 })
 
 app.listen( app.get('port') , () => {
@@ -74,6 +93,9 @@ function convertirXML(nombreArchivo,res){
 
     file.readFile(nombreArchivo, function (err, buffer) {
         parser.parseString(buffer,(err,result) =>{
+            datos = JSON.stringify(result)
+            res.send(datos)   
+            agregaDatabase(datos)
             res.send(JSON.stringify(result))
         })
     });
@@ -87,7 +109,9 @@ function convertirCSV(nombreArchivo,res){
     csvtojson().fromFile(nombreArchivo).on('json',(jsonObj)=>{
         coleccion.push(jsonObj)
     }).on('done',() =>{
-        res.send(JSON.stringify(coleccion))
+        datos = JSON.stringify(coleccion)
+        res.send(datos)   
+        agregaDatabase(datos)
         file.unlink(nombreArchivo)
     })
 
@@ -101,8 +125,9 @@ function convertirPdf(nombreArchivo,res){
             if(err){
              return console.log(err);
             }
-             res.send(JSON.stringify(rows))   
-            console.log(rows);
+             datos = JSON.stringify(rows)
+             res.send(datos)   
+             agregaDatabase(datos)
              file.unlink(nombreArchivo)
         });
     });
@@ -116,8 +141,9 @@ function convertirXls(nombreArchivo,res){
     if(err) {
         console.error(err);
     } else {
-        console.log(result)
-        res.send(JSON.stringify(result))   
+        datos = JSON.stringify(result);
+        res.send(datos)   
+        agregaDatabase(datos)
         file.unlink(nombreArchivo)
     }
     });
@@ -131,9 +157,16 @@ function convertirXLSX(nombreArchivo,res){
     if(err) {
         console.error(err);
     } else {
-        console.log(result)
-        res.send(JSON.stringify(result))   
+        datos = JSON.stringify(result);
+        res.send(datos)   
+        agregaDatabase(datos)
         file.unlink(nombreArchivo)
     }
     });
+}
+
+
+function agregaDatabase(datos){
+    dataBase.push(datos);
+    console.log(dataBase[0])
 }
