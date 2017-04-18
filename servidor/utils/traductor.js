32d293a0d90ae9a6222a35c0aef8f1732386cbdc
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2017-04-17 23:22:04 -0500

    refactorizacion en el traductor

--- servidor/utils/traductor.js
+++ servidor/utils/traductor.js
@@ -5,22 +5,33 @@ const translate = require('google-translate-api');
 
 function traductor(elementos) {
 
-    return Promise.all(elementos.map((obj) => {
+    return Promise.all(elementos.map((obj,i) => {
         return Promise.all(Object.keys(obj).map((key) => {
-            return traduceEsp(obj[key]).then(x => {
-                obj[key] = x
-                return obj
-            });
+            //Para eliminar el elemento [1] que se repite tras la traduccion aplicamos un filtro
+                return traduceEsp(obj[key]).then(x => {
+                    obj[key] = x
+                    console.log(obj)
+                    return obj
+                });
+            
         })).then(
-            x => {
-                return x.slice()
-                    .sort(function (a, b) {
-                        return a > b;
-                    })
-                    .reduce(function (a, b) {
-                        if (a.slice(-1)[0] !== b) a.push(b);
-                        return a;
-                    }, []);
+                x => {
+                    return x.filter(
+                        (x, key) => {
+                            if (key == 0) {
+                                return x;
+                            }
+                        }
+                    )
+                //Funcion anterior, menos eficiente pero funciona
+                /*                x.slice()
+                                    .sort(function (a, b) {
+                                        return a > b;
+                                    })
+                                    .reduce(function (a, b) {
+                                        if (a.slice(-1)[0] !== b) a.push(b);
+                                        return a;
+                                    }, []);*/
             }
             )
 
